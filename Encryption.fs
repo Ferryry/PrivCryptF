c2cb3e61d2b084f609b216d2b2c4c3d4b5b6a226
Riley Schneider <rileymika99@gmail.com>
2022-06-18 21:59:13 +0200

    Initial Commit

---
+++ Encryption.fs
@@ -0,0 +1,91 @@
+﻿module Encryption
+
+open System
+open System.Text
+open System.IO
+open System.Security.Cryptography
+
+type EncryptionBaseClass =
+
+    static member HashKey(input : string) : byte[] =
+        use sha256 = SHA256.Create()
+        sha256.ComputeHash(Encoding.UTF8.GetBytes(input))
+
+    static member GenerateSalt() : byte[] =
+        let data : byte array = Array.zeroCreate 32
+        use rng = new RNGCryptoServiceProvider()
+        for i = 1 to 10 do
+            rng.GetBytes(data)
+        data
+
+
+    static member Encrypt(file : string, key : string) : unit = 
+        let fs              = new FileStream(String.Format("{0}.aesfs", file), FileMode.Create)
+        let password        = EncryptionBaseClass.HashKey(key)
+        let salt            = EncryptionBaseClass.GenerateSalt()
+        
+        use aesAlg = Aes.Create()
+        aesAlg.KeySize      <- 256
+        aesAlg.BlockSize    <- 128
+        aesAlg.Padding      <- PaddingMode.PKCS7
+
+        let key = new Rfc2898DeriveBytes(key, salt, 1000)
+        aesAlg.Key          <- key.GetBytes(aesAlg.KeySize / 8)
+        aesAlg.IV           <- key.GetBytes(aesAlg.BlockSize / 8)
+
+        aesAlg.Mode         <- CipherMode.CFB
+
+        fs.Write(salt, 0, salt.Length)
+
+        let cs = new CryptoStream(fs, aesAlg.CreateEncryptor(), CryptoStreamMode.Write)
+        let fsIn = new FileStream(file, FileMode.Open)
+
+        let mutable buffer : byte array = Array.zeroCreate 1048576
+        let mutable read : int = Unchecked.defaultof<int> 
+
+        try
+            while (
+                read <- fsIn.Read (buffer, 0, buffer.Length)
+                read > 0) do
+                
+                cs.Write(buffer, 0, read)
+
+            fsIn.Close()
+        finally
+            cs.Close()
+            fs.Close()
+
+    static member Decrypt(file : string, key : string) : unit =
+        let fs              = new FileStream(file, FileMode.Open)
+        let password        = EncryptionBaseClass.HashKey(key)
+        let salt : byte[]   = Array.zeroCreate 32
+
+        fs.Read(salt, 0, salt.Length)
+
+        use aesAlg = Aes.Create()
+        aesAlg.KeySize      <- 256
+        aesAlg.BlockSize    <- 128
+
+        let key = new Rfc2898DeriveBytes(key, salt, 1000)
+        aesAlg.Key          <- key.GetBytes(aesAlg.KeySize / 8)
+        aesAlg.IV           <- key.GetBytes(aesAlg.BlockSize / 8)
+        aesAlg.Padding      <- PaddingMode.PKCS7;
+        aesAlg.Mode         <- CipherMode.CFB;
+
+        let cs = new CryptoStream(fs, aesAlg.CreateDecryptor(), CryptoStreamMode.Read)
+        let fsOut = new FileStream(Path.GetFileNameWithoutExtension(file), FileMode.Create)
+
+        let mutable buffer : byte array = Array.zeroCreate 1048576
+        let mutable read : int = Unchecked.defaultof<int> 
+
+        try
+            while (
+                read <- fsOut.Read (buffer, 0, buffer.Length)
+                read > 0) do
+                
+                cs.Write(buffer, 0, read)
+
+            fsOut.Close()
+        finally
+            cs.Close()
+            fs.Close()
---
+++ Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+open System.IO
+open Encryption
+
+[<EntryPoint>]
+let main args = 
+    try
+        if args.Length <> 3 then
+            raise (Exception("Wrong arguments.\nPlease use: [...].exe encrypt/decrypt dir password"))
+        else
+            if args[0].ToLower().Equals("encrypt") then
+                let directory = Directory.GetFiles(args[1])
+                for file in directory do
+                    EncryptionBaseClass.Encrypt(file, args[2])
+            elif args[0].ToLower().Equals("decrypt") then
+                let directory = Directory.GetFiles(args[1])
+                for file in directory do
+                    if file.EndsWith(".paul") then
+                        EncryptionBaseClass.Decrypt(file, args[2])
+                   
+    with
+        | ex -> Console.Error.WriteLine(String.Format("\nERROR: {0}\n", ex)); ()
+
+    0
