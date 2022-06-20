b7619b750a307528cc49df3ac42ec53d5eb9db8b
Riley Schneider <rileymika99@gmail.com>
2022-06-20 22:04:52 +0200

    Unused Math Implementation

---
+++ Math.fs
@@ -0,0 +1,9 @@
+﻿module Math
+
+open System
+
+let GetPercentage(value : float, max : float) : float =
+    Math.Round((100.0 / (max * value)), 2)
+
+let ByteToKiloByte(value : int) : float =
+    (float)(value / 1000)
